bf2c61298bc9b2c8e4fb9b9f22e978a513a343df
alexpatriquin <alexpatriquin@gmail.com>
2014-03-31 13:01:49 -0400

    implement devise and omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,31 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable
   devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable
-         
+
+  validates_presence_of :username
+  validates :username, :uniqueness => {:case_sensitive => false}
+
+  attr_accessor :login
+
+  validates :zipcode, numericality: { only_integer: true }, length: { is: 5 }
+  # validates :birthday
+  #http://stackoverflow.com/questions/18831869/rails-validate-type-date
+  #http://guides.rubyonrails.org/form_helpers.html#using-date-and-time-form-helpers
+
   has_many :user_poems
   has_many :poems, :through => :user_poems
 
   geocoded_by :zipcode
   after_validation :geocode, :if => :zipcode_changed?
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
 
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_create do |user|
@@ -36,7 +55,6 @@ class User < ActiveRecord::Base
   def email_required?
     super && provider.blank?
   end
-
 
   def update_with_password(params, *options)
     if encrypted_password.blank?
