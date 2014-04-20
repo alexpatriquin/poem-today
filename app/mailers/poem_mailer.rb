96021b759c8edf2c837724ad350920186d4aafcf
alexpatriquin <alexpatriquin@gmail.com>
2014-04-20 13:40:17 -0400

    Tweet delivery

--- app/mailers/poem_mailer.rb
+++ app/mailers/poem_mailer.rb
@@ -4,8 +4,12 @@ class PoemMailer < ActionMailer::Base
   def daily_email(user)
     @user = user
     @poem = PoemMatcher.new(user).match_poem
-    @url  = 'http://poemtoday.com/'
+    @poem_url  = "http://poemtoday.com#{poem_path(user.user_poems.last.poem_id)}"
     mail(to: @user.email, subject: 'Today\'s Poem')
+
+    if @user.twitter_handle
+      TWITTER_CLIENT.update("Good morning @#{@user.twitter_handle}, here is your poem for today. #{@poem_url}")
+    end
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140419023552) do
+ActiveRecord::Schema.define(version: 20140417011829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,15 +21,6 @@ ActiveRecord::Schema.define(version: 20140419023552) do
     t.string   "summary"
     t.integer  "min_temp"
     t.integer  "max_temp"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "keywords", force: true do |t|
-    t.string   "text"
-    t.integer  "frequency"
-    t.string   "source"
-    t.string   "source_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
