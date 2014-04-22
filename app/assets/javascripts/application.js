4938fb5dbc3df59016b55d6138205dbd0109ea65
alexpatriquin <alexpatriquin@gmail.com>
2014-04-22 14:09:38 -0400

    html5 progress bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,24 @@
 //= require_self
 //= require_tree . 
 
+$(document).ready(function() {  
+    var progressbar = $('#progressbar'),  
+        max = progressbar.attr('max'),  
+        time = (1000/max)*5,      
+        value = progressbar.val();  
+  
+    var loading = function() {  
+        value += 1;  
+        addValue = progressbar.val(value);  
+          
+        $('.progress-value').html(value + '%');  
+  
+        if (value == max) {  
+            clearInterval(animate);                      
+        }  
+    };  
+  
+    var animate = setInterval(function() {  
+        loading();  
+    }, time);  
+};
