a4fe686a33594232421065509e6da4c690691957
alexpatriquin <alexpatriquin@gmail.com>
2014-04-22 14:29:13 -0400

    progress bar as jqeury setInterval

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,30 +12,26 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery.ui.all
+//= require jquery.ui.progressbar
 //= require turbolinks
 //= require poems
 //= require_self
 //= require_tree . 
 
-$(document).ready(function() {  
-    var progressbar = $('#progressbar'),  
-        max = progressbar.attr('max'),  
-        time = (1000/max)*5,      
-        value = progressbar.val();  
-  
-    var loading = function() {  
-        value += 1;  
-        addValue = progressbar.val(value);  
-          
-        $('.progress-value').html(value + '%');  
-  
-        if (value == max) {  
-            clearInterval(animate);                      
-        }  
-    };  
-  
-    var animate = setInterval(function() {  
-        loading();  
-    }, time);  
-};
+$(document).ready(function(){
+  var progresspump = setInterval(function(){
+    /* query the completion percentage from the server */
+    $.get("/app/update-progress", function(data){
+      /* update the progress bar width */
+      $("#progress").css('width',data+'%');
+      /* and display the numeric value */
+      $("#progress").html(data+'%');
+
+      /* test to see if the job has completed */
+      if(data > 99.999) {
+        clearInterval(progresspump);
+        $("#progressouter").removeClass("active");
+        $("#progress").html("Done");
+      }
+    })
+  }, 1000);})
