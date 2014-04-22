e087bb72b33a05faf9b4bd67772a73245dfdde0d
alexpatriquin <alexpatriquin@gmail.com>
2014-04-22 14:43:13 -0400

    pace progress

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,25 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.progressbar
+//= require pace
 //= require turbolinks
 //= require poems
 //= require_self
-//= require_tree . 
-
-$(document).ready(function(){
-  var progresspump = setInterval(function(){
-    /* query the completion percentage from the server */
-    $.get("/app/update-progress", function(data){
-      /* update the progress bar width */
-      $("#progress").css('width',data+'%');
-      /* and display the numeric value */
-      $("#progress").html(data+'%');
-
-      /* test to see if the job has completed */
-      if(data > 99.999) {
-        clearInterval(progresspump);
-        $("#progressouter").removeClass("active");
-        $("#progress").html("Done");
-      }
-    })
-  }, 1000);})
+//= require_tree .
