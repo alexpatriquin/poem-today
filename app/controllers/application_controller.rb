14488ae13a7ef5e205d54c80dfc7d44e6fe80867
alexpatriquin <alexpatriquin@gmail.com>
2014-04-23 16:17:57 -0400

    less poems for markov

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
   end
 
   def ephemeral_poem?
-    session[:ephemeral_poem] && session[:ephemeral_poem].count > 5 #minimum is 4
+    session[:ephemeral_poem] && session[:ephemeral_poem].count > 4 #minimum is 4
   end
 
   def image_url(keyword)
