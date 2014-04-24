031b32c858c48125f2c32236c8f593b3cc9cc84d
alexpatriquin <alexpatriquin@gmail.com>
2014-04-24 16:15:08 -0400

    rm binding

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,11 +23,10 @@ class ApplicationController < ActionController::Base
     FlickRaw.api_key = ENV["FLICKR_API_KEY"]
     FlickRaw.shared_secret = ENV["FLICKR_API_SECRET"]
     begin
-      binding.pry
       flickr_photo = flickr.photos.search("text"=>"#{keyword}", "sort"=> "relevance").first
       @poem_image_url = "http://farm#{flickr_photo.farm}.staticflickr.com/#{flickr_photo.server}/#{flickr_photo.id}_#{flickr_photo.secret}.jpg"
     rescue
-      @poem_image_url = "https://s.yimg.com/pw/images/Panda_L.jpg"
+      @poem_image_url = ""
     end
   end
 
